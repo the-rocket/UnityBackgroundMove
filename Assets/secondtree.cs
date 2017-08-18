9e4f7fb304bf388f4c437acc7805bf9acce58d8d
Daniyar Kayirbolatov <daniyarkayirbolatov@gmail.com>
2017-08-18 17:21:06 -0500

    Background Initialized

---
+++ Assets/secondtree.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class secondtree : MonoBehaviour {
+
+	private static int times;
+	private Vector3 temp;
+	private Vector3 temp1;
+	// Use this for initialization
+	void Start () {
+		times = 0;
+		temp = temp1 = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		times++;
+		temp.x -= times * 0.01f;
+		if (temp.x < -20) {
+			temp.x = temp1.x = 19.99f;
+			times = 0;
+		}
+		transform.position = temp; 
+		temp = temp1;
+	}
+}
